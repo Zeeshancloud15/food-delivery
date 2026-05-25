4dbb49b9fb5e7afae05afadf049bcd468537f5ab
Md Zeeshan Uddin <zeeshancloud15@gmail.com>
2026-05-25 23:06:20 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
 
     // IMPORTANT: AWS RDS FIX
     String url =
-    "jdbc:mysql://foodapp.cxeakiucmdfw.eu-north-1.rds.amazonaws.com:3306/foodapp";
+    "jdbc:mysql://foodapp.cxeakiucmdfw.eu-north-1a.rds.amazonaws.com:3306/foodapp";
 
     String dbUser = "admin";
     String dbPass = "foodapp123";

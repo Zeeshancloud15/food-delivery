116519c4cafa7b86e139c9198fba78c954a5fa09
Md Zeeshan Uddin <zeeshancloud15@gmail.com>
2026-05-25 18:10:03 +0530

    Update App.java

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -5,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
     public static void main(String[] args) {
-
         SpringApplication.run(App.class, args);
     }
 }
